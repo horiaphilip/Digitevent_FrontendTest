610adaba6d9d237c7b4d901c9d500e7f666cc242
Horia <horiafr@gmail.com>
2026-05-31 14:24:45 +0300

    added log component for type

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,38 @@
 import { ref, onMounted } from "vue";
 import { fetchJoke, fetchFact } from "./services/api";
 import ContentCard from "./components/ContentCard.vue";
+import LogComp from "./components/LogComp.vue";
 
+export interface logType {
+  id: number;
+  type: "joke" | "fact";
+}
+//states+array for logs
 const joke = ref("");
 const fact = ref("");
+const logs = ref<logType[]>([]); //array for logs
+let nextId = 1;
+
+function addLog(type: "joke" | "fact") {
+  // add a new log to the beggining
+  logs.value.unshift({
+    id: nextId++,
+    type: type,
+  });
+}
 
 async function getJoke() {
   // fetch a joke
-
   const result = await fetchJoke();
   joke.value = result;
+  addLog("joke");
 }
 
 async function getFact() {
   //fetch a fact
   const result = await fetchFact();
   fact.value = result;
+  addLog("fact");
 }
 
 onMounted(() => {
@@ -25,12 +42,10 @@ onMounted(() => {
 });
 </script>
 
-//bind fetch w the get fct
-
 <template>
   <div>
     <ContentCard type="joke" :content="joke" @fetch="getJoke" />
-
     <ContentCard type="fact" :content="fact" @fetch="getFact" />
+    <LogComp :logs="logs" />
   </div>
 </template>
---
+++ src/components/LogComp.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { logType } from "../App.vue";
+
+defineProps<{
+  logs: logType[];
+}>();
+</script>
+
+///we take a loop to go through all the logs and show the type
+
+<template>
+  <div>
+    <h2>Logs</h2>
+    <div v-for="x in logs" :key="x.id">
+      <p>{{ x.type }}</p>
+    </div>
+  </div>
+</template>
